33fedc1b6a450d676a08cf59f5c9489206700410
katr6 <fomenko.k_ipz23@rcit.ukr.education>
2026-05-20 21:32:41 +0300

    feat: СР5 Впровадження узагальненого Repository<T> та алгоритмів на функціональних делегатах Action/Func

---
+++ TravelAgency.Domain/IRepository.cs
@@ -0,0 +1,11 @@
+namespace TravelAgency.Domain;
+
+public interface IRepository<T> where T : class
+{
+    void Add(T entity);
+    IReadOnlyList<T> GetAll();
+    
+    T? Find(Func<T, bool> predicate);
+    
+    void ExecuteForAll(Action<T> action);
+}
